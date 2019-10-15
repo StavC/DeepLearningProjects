5bb12a1eb72e92c6f1261acd964ce29cfcc55bc5
Stav Cohen <cohenstav1@gmail.com>
2019-10-15 15:06:49 +0300

    Starting to make the ANN in ChurnRateAtBank

--- ChurnRateAtBank/Main.py
+++ ChurnRateAtBank/Main.py
@@ -33,12 +33,15 @@ def main():
     x_test=sc.transform(x_test)   # transofrming the inputs for testing
 
     #### Make the ANN
-    hidden_layer_size=0
-    output_size=6 #input layer has 11 nodes and in output is 1 so (11+1)/2=6
+    hidden_layer_size=6 #input layer has 11 nodes and in output is 1 so (11+1)/2=6
+    output_size = 1 # output is 1
     model=tf.keras.Sequential()
-    model.add(tf.keras.layers.Dense(units=output_size, kernel_initializer='glorot_uniform',activation='relu')) #adding a Layer
-    model.add(tf.keras.layers.Dense(units=output_size, kernel_initializer='glorot_uniform',activation='relu')) #adding a Layer
+    model.add(tf.keras.layers.Dense(units=hidden_layer_size, kernel_initializer='glorot_uniform',activation='relu')) #adding a Layer
+    model.add(tf.keras.layers.Dense(units=hidden_layer_size, kernel_initializer='glorot_uniform',activation='relu')) #adding a Layer
+    model.add(tf.keras.layers.Dense(units=output_size, kernel_initializer='glorot_uniform',activation='sigmoid')) #sigmoid is good for 1 if there will be more than two categorize will choose softmax as activation
 
+    #### compiling the ANN
+    model.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
 
 
 
