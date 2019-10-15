d5f7dead986edcd194bc395c246a6e8ef96a5922
Stav Cohen <cohenstav1@gmail.com>
2019-10-15 14:58:15 +0300

    Starting to make the ANN in ChurnRateAtBank

--- ChurnRateAtBank/Main.py
+++ ChurnRateAtBank/Main.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.preprocessing import  LabelEncoder,OneHotEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import tensorflow as tf
 
 
 
@@ -25,11 +26,18 @@ def main():
     ###spliting the data
     x_train,x_test,y_train,y_test=train_test_split(inputs,targets,test_size=0.2,random_state=0)
     ########### x is the input for training and testing and y is the output for training and testing
+
     #### Feature Scaling
     sc=StandardScaler()
     x_train=sc.fit_transform(x_train) # fitting and transforming the inputs for training
     x_test=sc.transform(x_test)   # transofrming the inputs for testing
 
+    #### Make the ANN
+    hidden_layer_size=0
+    output_size=6 #input layer has 11 nodes and in output is 1 so (11+1)/2=6
+    model=tf.keras.Sequential()
+    model.add(tf.keras.layers.Dense(units=output_size, kernel_initializer='glorot_uniform',activation='relu')) #adding a Layer
+    model.add(tf.keras.layers.Dense(units=output_size, kernel_initializer='glorot_uniform',activation='relu')) #adding a Layer
 
 
 
